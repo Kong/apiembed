4d9def5f4644f87158ae763f4224e11bce0e4d3f
nijikokun <nijikokun@gmail.com>
2019-08-15 10:57:46 -0700

    fix(*) update libs, fix copy, fix overflow, fix http lib

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-'use strict'
+/* eslint-env node, mocha */
 
-var app = require('./app')
-var cluster = require('cluster')
-var os = require('os')
+"use strict";
 
-var WORKERS = process.env.WEB_CONCURRENCY || os.cpus().length
+var app = require("./app");
+var cluster = require("cluster");
+var os = require("os");
+
+var WORKERS = process.env.WEB_CONCURRENCY || os.cpus().length;
 
 if (cluster.isMaster) {
   // Spawn as many workers as there are CPUs in the system.
   for (var i = 0; i < WORKERS; i++) {
-    cluster.fork()
+    cluster.fork();
   }
 
-  cluster.on('exit', function (worker, code, signal) {
-    console.info('worker', worker.process.pid, 'died :(')
-    console.info('spawning a new worker')
-    cluster.fork()
-  })
+  cluster.on("exit", function(worker/*, code, signal*/) {
+    console.info("worker", worker.process.pid, "died :(");
+    console.info("spawning a new worker");
+    cluster.fork();
+  });
 } else {
-  app(function () {
-    console.info('spawning worker #' + cluster.worker.id)
-  })
+  app(function() {
+    console.info("spawning worker #" + cluster.worker.id);
+  });
 }
